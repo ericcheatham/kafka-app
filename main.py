5a7645704189cf0b0187f4d477f750429ba1ff11
Eric Cheatham <eric@meroxa.io>
2022-08-02 16:31:00 -0500

    test

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def passthrough(records: t.List[Record]) -> t.List[Record]:
+    print(f"cheatham kafka records: {records}")
     return records
 
 
@@ -18,12 +19,12 @@ class App:
         try:
             source = await turbine.resources("kafka-test")
 
-            records = await source.records("user_records", {})
+            records = await source.records("user_activity", {'conduit':'true', 'readFromBeginning': 'true'})
 
-            anonymized = await turbine.process(records, passthrough)
+            transform = await turbine.process(records, passthrough)
 
-            destination_db = await turbine.resources("webhook-dest")
+            destination_db = await turbine.resources("kafka-test")
 
-            await destination_db.write(anonymized, "collection_archive", {})
+            await destination_db.write(transform, "collection_archive", {'conduit':'true', 'topic': 'eric-test'})
         except Exception as e:
             print(e, file=sys.stderr)
