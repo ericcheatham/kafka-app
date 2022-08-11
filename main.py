c7f7d66041bfe94a6710e7ac032d62732ed5ef13
Eric Cheatham <eric@meroxa.io>
2022-08-11 11:16:27 -0500

    no func -> webhook

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import hashlib
 import logging
 import sys
-import typing as t
 
-from turbine.runtime import Record, Runtime
+from turbine.runtime import RecordList, Runtime
 
 logging.basicConfig(level=logging.INFO)
 
-
-def passthrough(records: t.List[Record]) -> t.List[Record]:
-    print(f"cheatham kafka records: {records}")
-    return records
+def print_records(records: RecordList) -> RecordList:
+    print(records)
+    return
 
 
 class App:
@@ -21,10 +19,10 @@ class App:
 
             records = await source.records("user_activity", {'conduit':'true', 'readFromBeginning': 'true'})
 
-            transform = await turbine.process(records, passthrough)
+            printed = await turbine.process(records, print_records)
 
-            destination_db = await turbine.resources("kafka-test")
+            destination_db = await turbine.resources("pg")
 
-            await destination_db.write(transform, "collection_archive", {'conduit':'true', 'topic': 'eric-test'})
+            await destination_db.write(printed, "messages")
         except Exception as e:
             print(e, file=sys.stderr)
